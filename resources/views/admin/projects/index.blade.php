303a4ef4d055de36dc867afeae83c530d4c508b3
silvialep <silvia.lepore_sl@libero.it>
2023-05-19 16:24:23 +0200

    first commit

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts/admin')
+
+@section('content')
+
+
+<table class="mt-5 table table-striped">
+  <thead>
+    <th>Titolo</th>
+    <th>Descrizione</th>
+    <th>Contenuto</th>
+    <th>Slug</th>
+    <th>Comandi</th>
+  </thead>
+
+  <tbody>
+
+    @foreach($projects as $project)
+    <tr >
+      <td>{{$project->title}}</td>
+      <td>{{$project->description}}</td>
+      <td>{{$project->content}}</td>
+      <td>{{$project->slug}}</td>
+      <td><a href="{{route('admin.projects.show', $project->slug)}}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
+    </tr>
+    @endforeach
+
+
+  </tbody>
+</table>
+@endsection
